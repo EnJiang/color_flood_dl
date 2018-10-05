7b689292dcdc2eae6b17f4ea29014ed1beb2feaa
EnJiang <315067671@qq.com>
2018-10-05 19:24:50 +0800

    disable logging

--- dqn.py
+++ dqn.py
@@ -63,7 +63,7 @@ dqn.compile(Adam(lr=.00025), metrics=['mae'])
 # slows down training quite a lot. You can always safely abort the training prematurely using
 # Ctrl + C.
 for _ in range(100):
-    dqn.fit(env, nb_steps=330000, visualize=False, verbose=1)
+    dqn.fit(env, nb_steps=330000, visualize=False, verbose=0)
 
     # After training is done, we save the final weights.
     dqn.save_weights('duel_dqn_{}_weights.h5f'.format(ENV_NAME), overwrite=True)
