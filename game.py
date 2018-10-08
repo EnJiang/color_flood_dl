b1b9ffea0ebc7772274c0ea7cd30890b07cf01d6
EnJiang <315067671@qq.com>
2018-10-08 12:40:25 +0800

    Update game.py
    it is now almost 2 times faster
    and it could be optimized further

--- game.py
+++ game.py
@@ -10,44 +10,41 @@ class Node():
         self.father = father
         self.status = 1
 
-    def explore(self, board, link):
+    def explore(self, board, link, visited):
         # 确定下一个探索点,进入下一个状态
         x, y = self.x, self.y
         check = None
         size = len(board)
-        if (self.status == 1 and self.x != size - 1):
+        if self.status == 1 and self.x != size - 1:
             check = [x + 1, y]
-        if (self.status == 2 and self.y != size - 1):
+        if self.status == 2 and self.y != size - 1:
             check = [x, y + 1]
-        if (self.status == 3 and self.x != 0):
+        if self.status == 3 and self.x != 0:
             check = [x - 1, y]
-        if (self.status == 4 and self.y != 0):
+        if self.status == 4 and self.y != 0:
             check = [x, y - 1]
         self.status = self.status + 1
 
         # 如果下一个探索点不存在或者下一个探索点已经在链表中,返回false,指示继续循环
         # 否则,如果下一个探索点为同色,返回check,指示退出循环;否则返回false,指示继续循环
-        if (check == None):
+        if check is None:
             # print self.status-1,self.x,',',self.y," edge"
             return False
-        exist = False
-        for n in link:
-            if (n.x == check[0] and n.y == check[1]):
-                exist = True
-        if (exist):
+        exist = (check[0], check[1]) in visited
+        if exist:
             # print self.status-1,self.x,',',self.y," exist"
             return False
-        if (board[0][0] == board[check[0]][check[1]]):
+        if board[0][0] == board[check[0]][check[1]]:
             # print self.status-1,self.x,',',self.y," child ",check
             return check
         else:
             # print self.status-1,self.x,',',self.y," not same"
             return False
 
-    def next(self, board, link):
+    def next(self, board, link, visited):
         check = False
         while (self.status != 5):
-            check = self.explore(board, link)
+            check = self.explore(board, link, visited)
             if (check):
                 break
         return check
@@ -57,6 +54,7 @@ class Spider():
     def __init__(self, board):
         # 定义链表,初始化蜘蛛在左上角第一个点
         self.link = [Node([0, 0], None)]
+        self.visited = set([(0, 0)])
         self.spider = self.link[0]
 
     def clean(self):
@@ -66,21 +64,18 @@ class Spider():
 
     def nodeExist(self, child):
         # 检查节点是不是已经存在于链表中
-        exist = False
-        for n in self.link:
-            if (n.x == child.x and n.y == child.y):
-                exist = True
-        return exist
+        return (child.x, child.y) in self.visited
 
     def target_board(self, board):
         # 定义链表,初始化蜘蛛在左上角第一个点
         size = len(board)
         self.link = [Node([0, 0], None)]
+        self.visited = set([(0, 0)])
         self.spider = self.link[0]
         # target_board = [[0 for x in range(size)] for y in range(size)]
         target_board = np.zeros(shape=(size, size), dtype=np.int)
         self.clean()
-        next = self.spider.next(board, self.link)
+        next = self.spider.next(board, self.link, self.visited)
         # 只要不是spider在原点且检查完了所有方向(这意味着整个棋盘已经检查完毕),就继续检查
         while (not (self.spider.father == None and next == False)):
             if (next):  # 若是存在下一个节点,则新建这个节点插入到链表里面去(如果其不存在于链表中的话),蜘蛛来到新节点
@@ -88,10 +83,11 @@ class Spider():
                 child = Node(next, father)
                 if (not self.nodeExist(child)):
                     self.link.append(child)
+                    self.visited.add((child.x, child.y))
                 self.spider = child
             else:  # 否则就让蜘蛛回到父节点去
                 self.spider = self.spider.father
-            next = self.spider.next(board, self.link)
+            next = self.spider.next(board, self.link, self.visited)
         # 最后,将每一个节点所对应的(x,y坐标设为target point)
         for n in self.link:
             target_board[n.x, n.y] = 1
@@ -216,14 +212,22 @@ class Game():
 
 
 if __name__ == "__main__":
+    import time
+
     test_list = []
-    for _ in range(1000):
+    start = time.time()
+    for _ in range(5000):
+        if _ % 1000 == 0:
+            print(_)
         game = Game(size=6)
 
         while not game.is_over():
             color = random.randint(1, 6)
             #  color = int(raw_input())
-            game.change(color, visual=True)
+            game.change(color, visual=False)
         test_list.append(game.step)
-    
+
     print(sum(test_list) / len(test_list))
+    end = time.time()
+
+    print((end - start) / 1000)
